3574e9a98d427036096583e78ec8f664718d4f5f
shessafridi <mshesskhan@gmail.com>
2021-10-01 22:50:38 +0500

    Added a loading animation

--- lib/src/pages/loading_page.dart
+++ lib/src/pages/loading_page.dart
@@ -1,29 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rive/rive.dart';
 import 'package:vidly/src/services/auth_service.dart';
 import 'package:vidly/src/services/navigation_service.dart';
 
-class LoadingPage extends StatelessWidget {
+class LoadingPage extends StatefulWidget {
   const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  late RiveAnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = SimpleAnimation('idle');
+    _controller.isActive = true;
+  }
 
   _checkAuthentication(BuildContext context) async {
     final router = context.read(navigationServiceProvider);
     var auth = context.read(authServiceProvider);
     var authenticated = await auth.checkAuth();
-    if (authenticated) {
-      router.fullyReplacyBy('/home');
-    } else {
-      router.fullyReplacyBy('/welcome');
-    }
+    Timer(const Duration(seconds: 3), () {
+      if (authenticated) {
+        router.fullyReplacyBy('/home');
+      } else {
+        router.fullyReplacyBy('/welcome');
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     _checkAuthentication(context);
     return const Scaffold(
-      body: Center(
-        child: Text("Loading please wait."),
+      body: RiveAnimation.asset(
+        'animations/loader.riv',
       ),
     );
   }
